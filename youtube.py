10de59600de5a3fd985844ee758e8ffdc2797e9c
Khushal020 <khushalgondaliya020@gmail.com>
2020-12-10 17:28:55 +0530

    completed

---
+++ youtube.py
@@ -0,0 +1,33 @@
+import sys
+import json
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+#from PyQt5.QtWebEngine import *
+#from PyQt5.QtWebKitWidgets import * 
+
+from PyQt5.QtWebEngineWidgets import QWebEngineView as QWebView,QWebEnginePage as QWebPage
+from PyQt5.QtWebEngineWidgets import QWebEngineSettings as QWebSettings
+class Youtube(QMainWindow):
+    
+    def __init__(self, page, parent=None):
+        super(Youtube, self).__init__(parent)
+        
+        self.setFixedSize(1200, 800)
+        
+        web = QWebView()
+        web.load(QUrl('http://' + page.lower()))
+        
+        view = QGraphicsView(self)
+        view.setFixedSize(1200, 800)
+        view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        
+        _layout = QVBoxLayout(view)
+        
+        scene = QGraphicsScene(self)
+        
+        _layout.addWidget(web)
+        view.setScene(scene)
+        
+  
